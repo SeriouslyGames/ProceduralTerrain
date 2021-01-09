3ac978f41278323550b8c392f13d02c62a6e612f
Mateo <mateotomasgomez@gmail.com>
2021-01-10 00:34:06 +0100

    Adds inverse operation

---
+++ Assets/ProceduralTerrain/NoiseGenerator/Operations/NoiseInverse.cs
@@ -0,0 +1,22 @@
+﻿using ProceduralTerrain.NoiseGenerator.Base;
+using UnityEngine;
+
+namespace ProceduralTerrain.NoiseGenerator
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "ProceduralTerrain/Noise/Operations/Inverse", order = 1)]
+    public class NoiseInverse : NoiseGeneratorBase
+    {
+        [SerializeField] private NoiseGeneratorBase noise;
+
+        // Start is called before the first frame update
+        public override float GetNoise(float x, float y, float z = 0)
+        {
+            if (noise != null ){
+                return( -noise.GetNoise(x, y , z )) ;
+            }
+            else{
+                return 0;
+            }
+        }
+    }
+}
